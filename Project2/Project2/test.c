bca265025a4f82b0ce14ab4763a6467f97618c86
shuai_sb_2b <1914390514@qq.com>
2024-10-10 16:28:36 +0800

    测试提交

--- Project2/Project2/test.c
+++ Project2/Project2/test.c
@@ -3,7 +3,10 @@
 #include <stdio.h>
 
 int main()
-{
-	printf("abc");
+{	
+	int a;
+	
+	scanf("%d", &a);
+	printf("%d",a);
 	return 0;
 }
---
+++ test_10_10/test_10_10/test.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+
+//int main()
+//{
+//	printf("Hello World\n");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("There are %d apples\n", 3);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("%s will come tonight\n", "zhangsan");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("%s says it is %d o'clock\n", "list", 21);
+//	return 0;
+//
+// }
+
+
+int main()
+{
+	printf("%15d\n", 123);
+	return 0;
+}

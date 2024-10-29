b295fb74dfc80bbc4a883dc5a82cbfc5a329e058
daijiaobu2049 <1914390514@qq.com>
2024-10-29 15:33:04 +0800

    static和extern

---
+++ test_10_29/test_10_29/add.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//int g_val = 2018;
+
+//static int g_val = 2018;
+
+
+
+
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+
+//static int Add(int x, int y)
+//{
+//	return x + y;
+//}

580342e188bec7a41c5830920b708290864ab45e
daijiaobu2049 <1914390514@qq.com>
2024-11-02 16:11:08 +0800

    扫雷1.1.1版

--- test_10_30_SaoLei/test_10_30_SaoLei/game.c
+++ test_10_30_SaoLei/test_10_30_SaoLei/game.c
@@ -53,7 +53,59 @@ void SetMine(char mine[ROWS][COLS], int row, int col)
 	}
 }
 
+//计算周围雷的个数
+//int GetMineCount(char mine[ROWS][COLS], int x, int y)
+//{
+//	return (mine[x - 1][y] + mine[x - 1][y - 1] + mine[x][y - 1] + mine[x + 1][y - 1] + mine[x + 1][y] + mine[x + 1][y + 1] + mine[x][y + 1] + mine[x - 1][y - 1] - 8 * '0');
+//}
+
+int GetMineCount(char mine[ROWS][COLS], int x, int y)
+{
+	int count = 0;
+	for (int i = -1; i <= 1; i++)
+	{
+		for (int j = -1; j <= 1; j++)
+		{
+			count += (mine[x + i][y + j] - '0');//字符减去'0'得到字符对应的数字
+		}
+	}
+	return count;
+}
+
 void FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col)
 {
-
+	int x = 0;
+	int y = 0;
+	int win = 0;
+	while (win < row * col - EASY_COUNT)
+	{
+		printf("请输入要排查雷的坐标:");
+		scanf("%d %d", &x, &y);
+		if (x >= 1 && x <=row && y >= 1 && y <= col)
+		{
+			//输入的位置是雷
+			if (mine[x][y] == '1')
+			{
+				printf("菜的发瘟，游戏结束\n");
+				DisplayBoard(mine, ROW, COL);
+				break;
+			}
+			else  //不是雷
+			{
+				int count = GetMineCount(mine, x, y);
+				show[x][y] = count + '0';//数字加'0'得到对应的字符
+				DisplayBoard(show, ROW, COL);
+				win++;
+			}
+		}
+		else
+		{
+			printf("输入的坐标有误x(1~9)y(1~9),请重新输入");
+		}
+	}
+	if (win == row * col - EASY_COUNT)
+	{
+		printf("恭喜排雷成功！\n");
+		DisplayBoard(mine, ROW, COL);
+	}
 }

a9f4cae83a710345a0c9a590b1440cb34856105f
daijiaobu2049 <1914390514@qq.com>
2024-11-04 21:08:29 +0800

    vs调试实用技巧

--- test_11_3/test_11_3/test.c
+++ test_11_3/test_11_3/test.c
@@ -2,8 +2,22 @@
 
 #include <stdio.h>
 
+//int main()
+//{
+//	printf("休息");
+//	return 0;
+//}
+
+#include <math.h> 
 int main()
 {
-	printf("休息");
+	float a = 0.0;
+	scanf("%f",&a);
+	float result = sqrt(a);
+
+	if ((a <= 1000) || (a > 0.0))
+		printf("%f", result);
+	else
+		printf("请重新输入");
 	return 0;
 }
---
+++ test_11_4/test_11_4/test.c
@@ -0,0 +1,70 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include  <stdio.h>
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int num = 100;
+//	char c = 'w';
+//
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//	}
+//	return 0;
+//}
+
+
+
+//写⼀个代码求n的阶乘
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int i = 1;
+//	int ret = 1;
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret *= i;
+//	}
+//	printf("%d\n", ret);
+//	return 0;
+//}
+//
+
+//如果n分别是求出每个数的阶乘，再求和就好了
+//在上⾯的代码上改造
+int main()
+{
+	int n = 0;
+	int i = 1;
+	int sum = 0;
+	for (n = 1; n <= 10; n++)
+	{
+		int ret = 1;
+		for (i = 1; i <= n; i++)
+		{
+			ret *= i;
+		}
+		sum += ret;
+	}
+	printf("%d\n", sum);
+	return 0;
+}
+//运⾏结果应该是错的？
+
+
+
+int main()
+{
+	int i = 0;
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	for (i = 0; i <= 12; i++)
+	{
+		arr[i] = 0;
+		printf("hehe\n");
+	}
+	return 0;
+}

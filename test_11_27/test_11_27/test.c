138c1188f5b96b1c52cef9114a9aecdd20be3aac
daijiaobu2049 <1914390514@qq.com>
2024-11-27 20:26:27 +0800

    指针-指针

---
+++ test_11_27/test_11_27/test.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//指针-指针
+
+//size_t my_strlen(char* p)
+//{
+//	size_t count = 0;//计数器
+//	while (*p != '\0')
+//	{
+//		count++;
+//		p++;
+//	}
+//	return count;
+//}
+
+//size_t my_strlen(char* p)
+//{
+//	char* start = p;
+//	char* end = p;
+//	while (*end /*!= '\0'*/)
+//	{
+//		end++;
+//	}
+//	return end - start;
+//}
+//
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	size_t len = my_strlen(arr);
+//	printf("%zd", len);
+//	return 0;
+//}
+
